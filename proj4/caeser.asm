ba8765b9b8ef1bf7df13e3dd659531438cd7eb4f
PeteePanda <scrandispeter@gmail.com>
2022-05-11 17:11:38 -0400

    comments and formating

--- proj4/caeser.asm
+++ proj4/caeser.asm
@@ -16,11 +16,11 @@ len_e		equ	$-encrypt
 new_line	db	10
 
 	section .bss
-arr	resq	10
+arr		resq	10
 string_buff:	resb	8
 string_len:	resb	8
 
-num_buff:		resb	3
+num_buff:	resb	3
 cipher_offset: 	resb	8	
 min_phrase_len: resb	30
 
@@ -35,33 +35,33 @@ start:
 	call	getlen
 	mov	qword[string_len], rcx
 
-	call	gshift	;print and store the shift value
+	call	gshift			;print and store the shift value
 
-	xor 	rcx, rcx	;clear rcx
+	xor 	rcx, rcx		;clear rcx
 
 	mov 	r10, qword[min_phrase_len]	;store the length in r10
 
-	mov 	rax, 1	;print prompt
+	mov 	rax, 1			;print prompt
 	mov 	rdi, 1
 	mov 	rsi, current
 	mov 	rdx, len_c
 	syscall
 
-	mov		rax, 1	;print original message
+	mov	rax, 1			;print original message
 	mov 	rdi, 1
-	mov		rsi, qword[string_buff]
-	mov		rdx, qword[string_len]
+	mov	rsi, qword[string_buff]
+	mov	rdx, qword[string_len]
 	syscall
 
-	mov		rax, 1
-	mov		rdi, 1
-	mov		rsi, new_line
-	mov		rdx, 1
+	mov	rax, 1
+	mov	rdi, 1
+	mov	rsi, new_line
+	mov	rdx, 1
 	syscall
 
 	xor 	rcx, rcx ;clear rcx
 	
-	call	 loop	;go to loop through the message and encrypt it
+	call	loop	;go to loop through the message and encrypt it
 
 	mov 	rax, 1	;print prompt
 	mov 	rdi, 1
@@ -69,16 +69,16 @@ start:
 	mov 	rdx, len_e
 	syscall
 
-	mov		rax, 1	;print encrypted message
+	mov	rax, 1	;print encrypted message
 	mov 	rdi, 1
-	mov		rsi, qword[string_buff]
-	mov		rdx, qword[string_len]
+	mov	rsi, qword[string_buff]
+	mov	rdx, qword[string_len]
 	syscall
 
-	mov		rax, 1
-	mov		rdi, 1
-	mov		rsi, new_line
-	mov		rdx, 1
+	mov	rax, 1
+	mov	rdi, 1
+	mov	rsi, new_line
+	mov	rdx, 1
 	syscall
 
 	ret
@@ -88,7 +88,7 @@ getlen:
 	add	rdi, rcx
 	cmp	byte[rdi], 0
 	je	return
-	inc rcx
+	inc 	rcx
 	jmp	getlen
 
 return:
@@ -97,21 +97,21 @@ return:
 gshift:
 
 	;prints get shift message
-	mov		rax, 1
-	mov		rdi, 1
-	mov		rsi, shift
-	mov		rdx, len_n
+	mov	rax, 1
+	mov	rdi, 1
+	mov	rsi, shift
+	mov	rdx, len_n
 	syscall
 
 	;storing shift
-	mov		rax, 0
-	mov		rdi, 0
-	mov		rsi, num_buff
-	mov		rdx, 3 ; double digit number + enter
+	mov	rax, 0
+	mov	rdi, 0
+	mov	rsi, num_buff
+	mov	rdx, 3 ; double digit number + enter
 	syscall
 
 	cmp 	byte[num_buff + 1], 10 ;checks if the second byte is a newline char
-	je 		working ;jumps to method for converting num_buff to numbers
+	je 	working ;jumps to method for converting num_buff to numbers
 	cmp 	byte[num_buff + 2], 10 ; checks if the third byte is a newline char
 	jne 	clearstr; jumps to method for clearing the shift variable if its not a newline char
 
@@ -123,7 +123,7 @@ working:
 	sub 	byte[num_buff + 1], 48 ;subtracting the second byte of numbuff = '2' by 48
 	mov 	al, byte[num_buff] ;move the first byte of num_buff into the last byte of rax(al)
 	cmp 	byte[num_buff + 1], 0
-	jl 		skipbyte2
+	jl 	skipbyte2
 
 	mov 	rdx, 10 ;set rdx to 10
 	mul 	rdx ;al = al * rdx
@@ -135,24 +135,24 @@ skipbyte2:
 	mov 	qword[cipher_offset], rax ;new 8 byte value is created to store the shift number
 
 	xor 	rax, rax
-	cmp		qword[cipher_offset], rax ;checks if the number is less than 0
-	jl		gshift ;restarts and gets new number for shift
+	cmp	qword[cipher_offset], rax ;checks if the number is less than 0
+	jl	gshift ;restarts and gets new number for shift
 	mov 	rax, 25
 	cmp 	qword[cipher_offset], rax ;checks if the number is greater than 25 
-	jg		gshift ;restarts and gets new number for shift
+	jg	gshift ;restarts and gets new number for shift
 
 	ret
 
 clearstr:
 	;goes through the num_buff until it is cleared (or hits \n)
 
-	mov		rax, 0
-	mov		rdi, 0
-	mov		rsi, num_buff ; gets the leftover char, if number was greater than 2 digits
-	mov		rdx, 1 ; double digit number + enter
+	mov	rax, 0
+	mov	rdi, 0
+	mov	rsi, num_buff ; gets the leftover char, if number was greater than 2 digits
+	mov	rdx, 1 ; double digit number + enter
 	syscall
-
-	cmp		byte[num_buff], 10 ;checks if the byte is a newline char
+	
+	cmp	byte[num_buff], 10 ;checks if the byte is a newline char
 	jne 	clearstr ; if not newline char, loops back to the next char
 	jmp 	gshift
 
@@ -160,49 +160,49 @@ loop:
 	; method used for checking if the char is uppercased or lowercased 
 	mov	rdi, qword[string_buff]
 	mov 	al, 'A'
-	cmp		byte[rdi + rcx], al
-	jl		incr 
+	cmp	byte[rdi + rcx], al
+	jl	incr 
 
 	mov 	al, 'Z'
 	cmp 	byte[rdi + rcx], al
-	jle		upper 
+	jle	upper 
 
 	mov 	al, 'a'
-	cmp		byte[rdi + rcx], al
-	jl		incr
+	cmp	byte[rdi + rcx], al
+	jl	incr
 
 	mov 	al, 'z'
-	cmp		byte[rdi + rcx], al
-	jle		lower
+	cmp	byte[rdi + rcx], al
+	jle	lower
 	jmp 	incr 
 
 incr:
 	; used for iterating through the original sentence
-	add		rcx, 1 ; moves which char the r8 is on, if r9 = 2, then r8 will be on the 3rd char, use for iterating through char spot
-	cmp		rcx, qword[string_len]
-	jl 		loop; loop for getting char spot
+	add	rcx, 1 ; moves which char the r8 is on, if r9 = 2, then r8 will be on the 3rd char, use for iterating through char spot
+	cmp	rcx, qword[string_len]
+	jl 	loop; loop for getting char spot
 	ret
 
 upper:
 	; adds the shift to the ascii value, if value is greater than 90 goes to subalpha
 	mov 	rax, qword[cipher_offset]
-	add		byte[rdi + rcx], al
+	add	byte[rdi + rcx], al
 	mov 	al, 'Z'
-	cmp		byte[rdi + rcx], al
-	ja		subalpha
+	cmp	byte[rdi + rcx], al
+	ja	subalpha
 	jmp 	incr
 
 lower:
 	; add the shift to the ascii value, if value is greater than 122 goes to subalpha
 	mov 	rax, qword[cipher_offset]
-	add		byte[rdi + rcx], al
+	add	byte[rdi + rcx], al
 	mov 	al, 'z'
-	cmp		byte[rdi + rcx], al
-	ja 		subalpha
+	cmp	byte[rdi + rcx], al
+	ja 	subalpha
 	jmp 	incr
 
 subalpha:
 	; subtracts 26 from the ascii value to loop back to the start of the alphabet
 	mov 	al, 26
-	sub		byte[rdi + rcx], al
+	sub	byte[rdi + rcx], al
 	jmp 	incr
--- proj4/main.asm
+++ proj4/main.asm
@@ -10,247 +10,247 @@ extern deallocate
 %define ARR_LEN 10
 %define ARR_SIZE 80 ;in bytes
 
-    section .data
-menu:		db  "Encryption menu options", 10, "s - show curent messages", 10, "r - read new messages", 10, "c - caesar cypher", 10, "f - frequency decrypt", 10, "q - quit program", 10, "enter letter option -> ", 0
-choice		equ $-menu
+	section .data
+menu:		db  	"Encryption menu options", 10, "s - show curent messages", 10, "r - read new messages", 10, "c - caesar cypher", 10, "f - frequency decrypt", 10, "q - quit program", 10, "enter letter option -> ", 0
+	
+choice		equ 	$-menu
 
 request:	db	"Enter string location: ", 0
 len_r		equ	$-request
 
-invalid:    db  "invalid option, try again"
-wrong       equ $-invalid
+invalid:    	db  	"invalid option, try again"
+wrong       	equ 	$-invalid
 
-string1:    db  "This is the original message.", 0
-stringLen:  equ $-string1
+string1:    	db  	"This is the original message.", 0
+stringLen:  	equ 	$-string1
 
-egg_msg:    db  "EASTER EGG! POGCHAMP", 0
-len_e   equ $-egg_msg
+egg_msg:    	db  	"EASTER EGG! POGCHAMP", 0
+len_e   	equ 	$-egg_msg
 
 new_line	db	10
 
-    section .bss
+	section .bss
 
-arr: resq ARR_LEN
+arr: 		resq 	ARR_LEN
 
 location_buff:	resb	2
-index:	resb	8
+index:		resb	8
 
 menu_buff:      resb    2
 string_buff:	resb	1000000
-num_buff:		resb	3
+num_buff:	resb	3
 replace_index:  resb    8
 
-z_count: resb 8
+z_count: 	resb 	8
 
 section .text
 
 	global 	main
 
 main:
-    push rbp
-	xor r8,r8
+	push 	rbp
+	xor 	r8,r8
     
 init:
-    ; allocate memory for new array, store value of r8 on stack
-    push r8
-    sub rsp, 8  ; align stack pointer
-    mov rdi, stringLen
-    call malloc
-    add rsp, 8  ; un-do stack operations
-    pop r8
-	mov qword[arr+r8], rax	;sets the first index to the allocated pointer
-    ; make a deep copy of the string
-    xor r9, r9
+	;allocate memory for new array, store value of r8 on stack
+	push 	r8
+	sub 	rsp, 8  		;align stack pointer
+	mov 	rdi, stringLen
+	call 	malloc			;allocate space to hold a deep copy of the og string
+	add 	rsp, 8  		;un-do stack pointer
+	pop 	r8
+	mov 	qword[arr+r8], rax	;sets the first index to the allocated pointer
+	
+	xor 	r9, r9
 strDeepCopyLoop:
-    mov bl, byte[string1 + r9]
-    mov byte[rax + r9], bl
-    inc r9
-    cmp r9, stringLen
-    jl strDeepCopyLoop
+	mov 	bl, byte[string1 + r9] 
+	mov 	byte[rax + r9], bl
+	inc 	r9
+	cmp 	r9, stringLen
+	jl 	strDeepCopyLoop
     
-	add r8, 8				;increments the index
-	cmp r8, ARR_SIZE    	;increments until it reaches the end
-	jl init
+	add 	r8, 8			;increments the index
+	cmp 	r8, ARR_SIZE    	;increments until it reaches the end
+	jl 	init
 
-
-    mov qword[replace_index], 0     ;position of what string to replace when reading
-    jmp gmenu
+	mov 	qword[replace_index], 0 ;position of what string to replace when reading
+	jmp 	gmenu
 
 gmenu:
-    mov rax, 1
-	mov rdi, 1
-	mov rsi, new_line
-	mov rdx, 1
+	mov 	rax, 1
+	mov 	rdi, 1
+	mov 	rsi, new_line		;prints the menu
+	mov 	rdx, 1
 	syscall
-    ;prints the menu
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, menu
-    mov rdx, choice
-    syscall
 
-    ;stores the user input in menu_buff
-    mov rax, 0
-    mov rdi, 0
-    mov rsi, menu_buff
-    mov rdx, 2
-    syscall ;be sure to check for more than 1 character, should en in \n
+	mov 	rax, 1
+	mov 	rdi, 1
+	mov 	rsi, menu		;asks for user input command
+	mov 	rdx, choice
+	syscall
 
-    cmp byte[menu_buff + 1], 10
-    jne toclear
+	mov 	rax, 0
+	mov 	rdi, 0
+	mov 	rsi, menu_buff		;stores the user input in num buff
+	mov 	rdx, 2
+	syscall
+
+	cmp 	byte[menu_buff + 1], 10	;if there is more than one character in menu_buff then it clears the remaining inputs
+	jne 	toclear
 
 menuaction:
-    cmp byte[menu_buff], 'z'
-    je  incz
-    cmp byte[menu_buff], 'Z'
-    je  incz
+	cmp 	byte[menu_buff], 'z' 	;if the input is z then jumps to incz
+	je  	incz
+	cmp 	byte[menu_buff], 'Z'
+	je  	incz
 
-    mov byte[z_count], 0
+	mov 	byte[z_count], 0 	;resets the number of times z was incremented
 
-    cmp byte[menu_buff], 's'
-    je  calldisplay
-    cmp byte[menu_buff], 'S'
-    je  calldisplay
+	cmp 	byte[menu_buff], 's' 	;if the input was s then it jumps to calldisplay
+	je  	calldisplay
+	cmp 	byte[menu_buff], 'S'
+	je  	calldisplay
+					
+	cmp 	byte[menu_buff], 'r' 	;if the input was r then it jumps to callread
+	je  	callread
+	cmp 	byte[menu_buff], 'R'
+	je  	callread
 
-    ;jumps to callread if user inputs r or R
-    cmp byte[menu_buff], 'r'
-    je  callread
-    cmp byte[menu_buff], 'R'
-    je  callread
+	cmp 	byte[menu_buff], 'c' 	;if the input was c then it jumps to gstring
+	je  	gstring
+	cmp 	byte[menu_buff], 'C'
+	je  	gstring
 
-    cmp byte[menu_buff], 'c'
-    je  gstring
-    cmp byte[menu_buff], 'C'
-    je  gstring
+	cmp 	byte[menu_buff], 'f' 	;if the input was f then it jumps to calldecrypt
+	je  	calldecrypt
+	cmp 	byte[menu_buff], 'F'
+	je  	calldecrypt
 
-    cmp byte[menu_buff], 'f'
-    je  calldecrypt
-    cmp byte[menu_buff], 'F'
-    je  calldecrypt
-
-    ;jumps to exit if user inputs q or Q
-    cmp byte[menu_buff], 'q'
-    je exit
-    cmp byte[menu_buff], 'Q'
-    je exit
+	cmp 	byte[menu_buff], 'q' 	;if the input was q then it jumps to exit
+	je 	exit
+	cmp 	byte[menu_buff], 'Q'
+	je 	exit
 
 toclear:
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, invalid
-    mov rdx, wrong
-    syscall
-
-    mov rax, 1
-	mov rdi, 1
-	mov rsi, new_line
-	mov rdx, 1
+	mov 	rax, 1
+	mov 	rdi, 1
+	mov 	rsi, invalid		;tells the user that their input was invalid
+	mov 	rdx, wrong
 	syscall
 
-    cmp byte[menu_buff + 1], 10 ;checks if the byte is a newline char
-	jne clearing ; if not newline char, loops back to the next char
-	jmp gmenu
+	mov 	rax, 1
+	mov 	rdi, 1
+	mov 	rsi, new_line		;prints a new line
+	mov 	rdx, 1
+	syscall
+
+	cmp 	byte[menu_buff + 1], 10 ;checks if the byte is a newline char
+	jne 	clearing 		;if not newline char, loops back to the next char
+	jmp 	gmenu
 
 clearing:
-    mov rax, 0
-	mov rdi, 0
-	mov rsi, menu_buff ; gets the leftover char, if number was greater than 2 digits
-	mov rdx, 1 ; double digit number + enter
+	mov 	rax, 0
+	mov 	rdi, 0
+	mov 	rsi, menu_buff 		;gets the leftover char, if input was greater than 1 character
+	mov 	rdx, 1
 	syscall
 
-	cmp byte[menu_buff], 10 ;checks if the byte is a newline char
-	jne clearing ; if not newline char, loops back to the next char
-	jmp gmenu
+	cmp 	byte[menu_buff], 10 	;checks if the byte is a newline char
+	jne 	clearing 		;if not newline char, loops back to the next char
+	jmp 	gmenu
 
 calldisplay:
-    xor rdi, rdi
-    mov rdi, arr
-    call display
-    jmp gmenu
+	xor 	rdi, rdi		;clears rdi
+	mov 	rdi, arr		;stores arr in rdi as the first parameter in the c function
+	call 	display			;calls the display c function
+	jmp 	gmenu			;jumps back to gmenu for the next input
 
 callread:
-    mov rdi, arr
-    xor rsi, rsi
-    mov rsi, qword[replace_index]
-    call read
-    mov qword[replace_index], rax
-    cmp qword[replace_index], ARR_LEN
-    jae modTen
-    jmp gmenu
+	xor	rdi, rdi		;clears rdi
+	mov 	rdi, arr		;stores arr in rdi as the first parameter
+	xor 	rsi, rsi		;clears rsi and stores the index where to replace the string
+	mov 	rsi, qword[replace_index]
+	call 	read			;calls the read c function and stores the returned index in replace_index
+	mov 	qword[replace_index], rax
+	cmp 	qword[replace_index], ARR_LEN
+	jae 	modTen			;%10 the replace_index if it is greater than the length of the array
+	jmp 	gmenu
 
 gstring:
-	mov		rax, 1	;prints the request for the original string
-	mov		rdi, 1
-	mov		rsi, request
-	mov		rdx, len_r
+	mov	rax, 1			;prints the request for the original string
+	mov	rdi, 1
+	mov	rsi, request
+	mov	rdx, len_r
 	syscall
 
-	mov		rax, 0	;stores the original string
-	mov		rdi, 0
-	mov		rsi, location_buff
-	mov		rdx, 2
+	mov	rax, 0			;stores the original string
+	mov	rdi, 0
+	mov	rsi, location_buff
+	mov	rdx, 2
 	syscall
 
-	cmp byte[location_buff + 1], 10
-    jne toclear
+	cmp 	byte[location_buff + 1], 10
+	jne 	toclear			;tells the user that their input is invalid if they input a location more than 1 character
 
 tofix:
-    sub byte[location_buff], 48
-    xor rax, rax
-    cmp byte[location_buff], al
-    jl  gstring
+	sub 	byte[location_buff], 48
+	xor 	rax, rax
+	cmp 	byte[location_buff], al
+	jl  	gstring			;asks for a new location if the input is less than ASCII 0
 
-    mov al, 9
-    cmp byte[location_buff], al
-    jg  gstring
+	xor	rax, rax
+	mov 	al, 9
+	cmp 	byte[location_buff], al
+	jg  	gstring			;asks for a new location if the input is greater than ASCII 9
 
 storeindex:
-    mov al, byte[location_buff]
-	mov	qword[index], rax
+	mov 	al, byte[location_buff]
+	mov	qword[index], rax 	;stores the users desired index in index
 
 	mov	rax, 8
-	mul	qword[index]
-	mov	qword[index], rax
+	mul	qword[index]		;multiplies the index by 8 to get the proper index in assembly since it's dog
+	mov	qword[index], rax	;stores the index in index
 
 callceaser:
-    xor rdi, rdi
-    mov rcx, qword[index]
-    mov rdi, qword[arr + rcx]
-    call start
-    jmp gmenu
+	xor 	rdi, rdi		;clears rdi
+	mov 	rcx, qword[index]	;stores the index in rcx
+	mov 	rdi, qword[arr + rcx]	;moves the array at index rcx into rdi
+	call 	start			;calls the start assembly lable in caeser.asm
+	jmp 	gmenu			;jumps to menu for new input
 
 calldecrypt:
-    xor rdi, rdi
-    mov rdi, arr
-    call decrypt
-    jmp gmenu
+	xor 	rdi, rdi		;clears rdi
+	mov 	rdi, arr		;moves the array into rdi
+	call 	decrypt			;calls the decrypt c function
+	jmp 	gmenu			;jumps to menu for new input
 
 modTen:
-    sub qword[replace_index], ARR_LEN
-    jmp gmenu
+	sub 	qword[replace_index], ARR_LEN
+	jmp 	gmenu			;subtracts the replace index for read by 10 if the replace index is greater than 10
 
 incz:
-    inc qword[z_count]
-    cmp qword[z_count], 4
-    je  egg
-    jmp toclear
+	inc 	qword[z_count]		;increments z_count by 1
+	cmp 	qword[z_count], 4	;if z_count is 1 then jumps to egg lable
+	je  	egg
+	jmp 	toclear
 
 egg:
-    mov qword[z_count], 0
+	mov 	qword[z_count], 0 	;resets the z_count
 
-    mov		rax, 1	;prints the request for the original string
-	mov		rdi, 1
-	mov		rsi, egg_msg
-	mov		rdx, len_e
+	mov	rax, 1
+	mov	rdi, 1
+	mov	rsi, egg_msg		;prints the easter egg message
+	mov	rdx, len_e
 	syscall
 
-    jmp gmenu
+	jmp 	gmenu			;jumps back to gmenu
 
 exit:
-    ; free the dynamically allocated memory
-    xor rdi, rdi
-    mov rdi, arr
-    call deallocate
-	mov rax, 60
-	xor rdi, rdi
+					;free the dynamically allocated memory before exiting
+	xor 	rdi, rdi		;clears rdi
+	mov 	rdi, arr		;moves the array into rdi
+	call 	deallocate		;calls the deallocate c function
+	mov 	rax, 60			;exits
+	xor 	rdi, rdi
 	syscall
+	
